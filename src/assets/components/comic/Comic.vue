090bf190be2990172451337ec56ba49fdba55466
Adan Florez <adan.florezb@gmail.com>
2021-11-27 16:59:25 -0500

    feat: get comics service

---
+++ src/assets/components/comic/Comic.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+import comicServices from '@/http/services';
+
+export default Vue.extend({
+  mounted() {
+    this.getComics();
+  },
+  methods: {
+    async getComics() {
+      const data = await comicServices.getComic();
+      console.log(data);
+    }
+  }
+});
+</script>
+
+<style scoped></style>
