a8486f11b943c3af5b72b58fef673173fd4e1e38
Adan Florez <adan.florezb@gmail.com>
2021-11-27 10:17:07 -0500

    style: unused styles were removed

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,4 @@
 
 <style lang="scss">
 @import '@/assets/scss/index';
-
-#app {
-  font-family: 'Lato', 'sans-serif' !important;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
