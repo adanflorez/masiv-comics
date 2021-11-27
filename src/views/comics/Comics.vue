45cd23ce84ca66bc07e54f283e51fb36c38634b4
Adan Florez <adan.florezb@gmail.com>
2021-11-27 18:04:34 -0500

    feat: getting comics from API

--- src/views/comics/Comics.vue
+++ src/views/comics/Comics.vue
@@ -1,11 +1,36 @@
 <template>
-  <div>Comic</div>
+  <div class="my-6">
+    <Comic :comic="comic" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+// components
+import Comic from '@/components/comic/Comic.vue';
+// services
+import comicServices from '@/http/services';
+// interfaces
+import IComic from '@/interfaces/comic';
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { Comic },
+  data: () => ({
+    comic: {} as IComic
+  }),
+  mounted() {
+    this.getComics();
+  },
+  methods: {
+    /**
+     * get comics from https://xkcd.com/ API
+     */
+    async getComics() {
+      const res = await comicServices.getComic();
+      this.comic = res.data;
+    }
+  }
+});
 </script>
 
 <style scoped></style>
