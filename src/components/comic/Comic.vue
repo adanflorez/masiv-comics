9718de53055b6b99a8a8d41d064afd8436abf648
Adan Florez <adan.florezb@gmail.com>
2021-11-27 19:48:38 -0500

    feat: vue star rating in component

--- src/components/comic/Comic.vue
+++ src/components/comic/Comic.vue
@@ -12,6 +12,20 @@
       >
         <img :data-src="comic.img" :alt="comic.alt" />
       </div>
+      <div class="flex justify-center mt-8">
+        <StarRating
+          v-model="rating"
+          :show-rating="false"
+          :border-width="4"
+          border-color="#d8d8d8"
+          :rounded-corners="true"
+          :star-points="[
+            23, 2, 14, 17, 0, 19, 10, 34, 7, 50, 23, 43, 38, 50, 36, 34, 46, 19,
+            31, 17
+          ]"
+          @rating-selected="setRating"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -20,8 +34,13 @@
 import Vue, { PropOptions } from 'vue';
 // interfaces
 import IComic from '@/interfaces/comic';
+// components
+import StarRating from 'vue-star-rating';
 
 export default Vue.extend({
+  components: {
+    StarRating
+  },
   props: {
     /**
      * Comic object
@@ -31,12 +50,23 @@ export default Vue.extend({
       type: Object
     } as PropOptions<IComic>
   },
+  data: () => ({
+    rating: 0
+  }),
   computed: {
     /**
      * returns a generic image in case of API error
      */
     placeholder(): string {
       return require('@/assets/img/placeholder.png');
+    }
+  },
+  methods: {
+    /**
+     * Set rating of current comic
+     */
+    setRating(rating: number) {
+      console.log('id', this.comic.num, rating);
     }
   }
 });
