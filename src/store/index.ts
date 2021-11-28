2234c3a2c2368e9073fb33ffa51007f8787e26c1
Adan Florez <adan.florezb@gmail.com>
2021-11-28 12:12:44 -0500

    feat: vuex persist config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import ComicModule from '@/store/comic';
+import vuexLocal from '@/plugins/vuex-persist';
 
 Vue.use(Vuex);
 
@@ -10,5 +11,6 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     comic: ComicModule
-  }
+  },
+  plugins: [vuexLocal.plugin]
 });
