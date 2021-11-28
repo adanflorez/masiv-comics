277e61d2a6389837f70af6e5a8bf9d520ebc4301
Adan Florez <adan.florezb@gmail.com>
2021-11-28 16:21:38 -0500

    feat: buttons to navigate between comics

--- src/views/comics/Comics.vue
+++ src/views/comics/Comics.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="my-6">
     <Comic :comic="current" />
+    <div class="flex justify-center mt-8">
+      <Button
+        class="mx-2"
+        :text="'Anterior'"
+        @click.native="nextOrPrevComic(false)"
+      />
+      <Button class="mx-2" :text="'Aleatorio'" />
+      <Button
+        class="mx-2"
+        :text="'Siguiente'"
+        @click.native="nextOrPrevComic(true)"
+      />
+    </div>
   </div>
 </template>
 
@@ -8,12 +21,13 @@
 import Vue from 'vue';
 // components
 import Comic from '@/components/comic/Comic.vue';
+import Button from '@/components/common/ui/button/Button.vue';
 // services
 import comicServices from '@/http/services';
 import { mapActions, mapState } from 'vuex';
 
 export default Vue.extend({
-  components: { Comic },
+  components: { Comic, Button },
   computed: {
     ...mapState('comic', ['current'])
   },
@@ -36,13 +50,35 @@ export default Vue.extend({
       }
     },
     /**
-     * comic actions
+     * Next or previous comic page, depends of isNext
+     *
+     * @param {Boolean} isNext
+     */
+    nextOrPrevComic(isNext: boolean) {
+      const comicId = Number(this.$route.params.id);
+      if (isNext) {
+        this.$router.push(`/comic/${comicId + 1}`);
+      } else {
+        console.log('aca fueee');
+        this.$router.push(`/comic/${comicId - 1}`);
+      }
+    },
+    /**
+     * Store actions
      */
     ...mapActions({
       setCurrentComic: 'comic/setCurrentComic',
       showLoading: 'loading/showLoading',
       hideLoading: 'loading/hideLoading'
     })
+  },
+  watch: {
+    '$route.params.id': {
+      handler() {
+        this.getComics();
+      },
+      deep: true
+    }
   }
 });
 </script>
