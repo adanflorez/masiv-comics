c5d42faafd1f19486afe9ad03e6e72d2abaeaddf
Adan Florez <adan.florezb@gmail.com>
2021-11-28 16:19:57 -0500

    feat: button component

---
+++ src/components/common/ui/button/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="border-none bg-dark-slate p-3 text-base text-default-secondary hover:text-default-smoke"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    /**
+     * text in button
+     */
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+});
+</script>
+
+<style scoped></style>
